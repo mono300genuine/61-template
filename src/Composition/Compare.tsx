81d138ad315abc7eeaa529dcf5329e9499c3c274
Raysayhey <rlotmar@hotmail.com>
2024-10-14 15:28:40 +0200

    compare feature

---
+++ src/Composition/Compare.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react'
+import { AbsoluteFill, OffthreadVideo, staticFile } from 'remotion'
+
+import { HEIGHT, WIDTH } from '../lib/consts'
+import Main from './Composition'
+
+export const Compare: React.FC = (props: any) => {
+  return (
+    // insert Name of your video from public/examples/ folder 
+    <CompareWithVideo example={staticFile(`/examples/${"YourTemplateHere"}.mp4`)}>
+      <Main {...props} />
+    </CompareWithVideo>
+  )
+}
+
+
+export const COMPARE = process.env.COMPARE as 'x' | 'y' | undefined
+
+export const CompareWithVideo = ({ example, children }: { example: string; children: ReactNode }) => {
+  return (
+    <AbsoluteFill style={{ overflow: 'visible', width: '100%', height: '100%' }}>
+      {children}
+      {COMPARE && (
+        <AbsoluteFill style={{ zIndex: 50 }}>
+          <OffthreadVideo
+            src={example}
+            onError={console.log}
+            style={{
+              marginTop: 'auto',
+              marginLeft: 'auto',
+              background: 'white',
+              height: HEIGHT,
+              width: WIDTH,
+            }}
+          />
+        </AbsoluteFill>
+      )}
+    </AbsoluteFill>
+  )
+}
